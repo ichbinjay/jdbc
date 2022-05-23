e5284ec97ac453d9b6abd329fa7d53f7a9aefdfd
ichbinjay <bvred926@gmail.com>
2022-05-23 18:11:40 +0530

    \

---
+++ src/Select.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JDBC;
+import java.sql.*;
+public class Select{
+    public static void main(String args[]){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc","root","root");
+            
+            Statement stmt = con.createStatement();
+            
+            ResultSet rs = stmt.executeQuery("select * from student");
+            
+            while(rs.next()){
+                System.out.println(rs.getInt(1)+" "+rs.getString(2)+" "+rs.getString(3));
+            }
+            con.close();
+        }catch(Exception e){
+            System.out.print(e);
+        }
+    }
+}
